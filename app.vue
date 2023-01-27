352e1e284a1bea462843f5fb229e4cfbd80bf3d7
junaidsahar <imaadahmed300@gmail.com>
2023-01-27 18:41:21 +0500

    minor changes

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
 <template>
+
+  <!-- if you want to create multi pages website than just create pages folder at root directary and create 'index.vue' and delete 'app.vue' file -->
+
+
   <div class="flex justify-center items-center h-screen bg-emerald-600">
     <h3 class="text-white font-bold text-3xl">YOU ARE ALL SET NOW :)</h3>
   </div>
